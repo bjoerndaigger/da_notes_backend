30596c3336b35c6fe0a6fc09f983c9b10a1dacfc
bjoerndaigger <bjoern.daigger@me.com>
2025-10-06 16:12:53 +0200

    add success message to note delete endpoint

--- notes_app/api/views.py
+++ notes_app/api/views.py
@@ -1,8 +1,17 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from notes_app.models import Note
+from rest_framework.response import Response
 from .serializers import NoteSerializer
 
 
 class NotesViewSet(viewsets.ModelViewSet):
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        note = self.get_object()
+        note.delete()
+        return Response(
+            {"message": "Note wurde erfolgreich gelöscht."},
+            status=status.HTTP_204_NO_CONTENT
+        )
